5e97c2444f5ebf496527eb0a71a697c6222432c8
Ablene1 <111275457+Ablene1@users.noreply.github.com>
2023-03-24 19:26:51 +0900

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# install puppet-lint using Puppet
-
-package { 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'gem',
+# install puppet-lint
+package{'puppet-lint':
+ensure   => '2.5.0',
+provider => 'gem'
 }
