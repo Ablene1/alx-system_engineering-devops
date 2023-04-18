b9ac0c4d04389814c5a54e9925558f13d40bff68
Ablene1 <111275457+Ablene1@users.noreply.github.com>
2023-04-19 01:33:43 +0900

    Delete 0x14-mysql directory

--- 0x14-mysql/README.md
+++
@@ -1,6 +0,0 @@
-# 0x14-mysql
-
-## Install mysql
-
-`sudo apt-get install mysql-server mysql-client`
-
--- 0x14-mysql/create-replica-user.sql
+++
@@ -1,8 +0,0 @@
--- Create user to check DB status 
--- Run on the master server
-
-CREATE USER IF NOT EXISTS 'replica_user'@'%'
-IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT  REPLICATION CLIENT, SELECT ON *.*
-TO 'replica_user'@'%';
-FLUSH PRIVILEGES;
